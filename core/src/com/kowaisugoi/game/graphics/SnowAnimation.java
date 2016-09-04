6bb06dcdc00cf90a43dadf52e1fde48c73e31c1a
gjhutchison <grahamjhutchison@gmail.com>
2016-09-03 18:49:09 -0600

    Initial creation of a snow animation object

---
+++ core/src/com/kowaisugoi/game/graphics/SnowAnimation.java
@@ -0,0 +1,25 @@
+package com.kowaisugoi.game.graphics;
+
+
+public class SnowAnimation {
+
+    private int _stormSize;
+    private int _stormIntensity;
+
+    public void SnowAnimation(int size, float intensity) {
+
+    }
+
+    private class snowflake {
+        private float _xLocation;
+        private float _yLocation;
+
+        private float _speed;
+
+        public snowflake(int size, float curX, float curY) {
+
+        }
+    }
+}
+
+
