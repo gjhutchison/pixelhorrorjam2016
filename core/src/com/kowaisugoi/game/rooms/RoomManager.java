6e37995b51e7371450d036524b4e9e5c76ff886e
gjhutchison <gjhutchi@ucalgary.ca>
2016-09-12 20:29:59 -0400

    Added hallway room

--- core/src/com/kowaisugoi/game/rooms/RoomManager.java
+++ core/src/com/kowaisugoi/game/rooms/RoomManager.java
@@ -11,9 +11,11 @@ public final class RoomManager {
 
     public RoomManager() {
         _roomMap.put(RoomId.MAIN_HALL, new RoomMainHall());
+        _roomMap.put(RoomId.HALLWAY, new RoomHallway());
         _roomMap.put(RoomId.FRONT_DOOR_INTERIOR, new RoomFrontDoorInterior());
         _roomMap.put(RoomId.FRONTYARD, new RoomFrontYard());
         _roomMap.put(RoomId.CAR, new RoomCar());
+
     }
 
     public static Map<RoomId, Room> getRoomMap() {
